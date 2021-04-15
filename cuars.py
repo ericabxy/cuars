bea47bc578f1e86627998ce6ac8e1558a19c71fe
Eric Duhamel <ericxdu23@gmail.com>
2021-04-15 00:05:50 -0700

    add license

--- cuars.py
+++ cuars.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python3
+'''
+Copyright 2021 Eric Duhamel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import datetime
 import os
 import re
@@ -7,14 +25,14 @@ from PIL import Image, ImageDraw, ImageFont
 
 def main():
     print("Invoked " + sys.argv[0])
+    print("cuars: creating interface")
     inter = Interface(135, 240)
-    isotime = datetime.datetime.now().replace(microsecond=0).isoformat()
-    filename = "".join(re.split("-|T|:", isotime)) + ".example.png"
     if len(sys.argv) > 1:
-        print("cuars: creating interface")
         if os.path.isdir(sys.argv[1]):
+            print("cuars: displaying table of contents for " + sys.argv[1])
             image = inter.get_directory(sys.argv[1])
         else:
+            print("cuars: displaying table of " + sys.argv[1])
             if len(sys.argv) > 2:
                 shades = sys.argv[2].split(",")
                 scheme = []
@@ -22,15 +40,14 @@ def main():
                     scheme.append(int(s))
                 inter.set_scheme(scheme)
             image = inter.get_table(sys.argv[1].split(","))
-        image.save(filename)
-        print("cuars: saved image to " + filename)
     else:
-        print("cuars: no directory given")
-        print("cuars: try invoking with \"test.dir\" on the command line")
-        print("cuars: saving an test image to " + filename)
+        print("cuars: drawing an example display")
         image = inter.get_table(("node 1", "node 2", "node 3", "node 4",
                                  "node 5",))
-        image.save(filename)
+    isotime = datetime.datetime.now().replace(microsecond=0).isoformat()
+    filename = "".join(re.split("-|T|:", isotime)) + ".example.png"
+    image.save(filename)
+    print("cuars: saved screenshot to " + filename)
 
 class Interface():
     def __init__(self, width, height):
