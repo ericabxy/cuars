206c12d8966c93a37831c142a699faf2da644b75
Eric Duhamel <ericxdu23@gmail.com>
2021-05-15 17:44:36 -0700

    interactive tkinter test

--- testwin.py
+++ testwin.py
@@ -9,26 +9,29 @@ import cuars
 
 # Figure out which directory we're gonna show
 if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
-    basedir = sys.argv[1]
+    dirname = sys.argv[1]
 else:
-    basedir = os.getcwd()
-here = os.path.dirname(__file__)
+    dirname = os.getcwd()
 
 interf = cuars.Interface(200, 150)
+mark = 0
 
 window = tk.Tk()
 canvas = tk.Canvas(window)
-btn_a = tk.Button(window, text="A")
-btn_b = tk.Button(window, text="B")
 canvas.pack()
+def refresh():
+    global image, mark
+    list, shades = cuars.get_directory(dirname)
+    mark = (mark+1)%len(list)
+    interf.name = os.path.basename(dirname)
+    interf.show_badges(list, (0, 0), shades, mark)
+    image = interf.get_tkimage()
+    canvas.create_image((0, 0), anchor=tk.NW, image=image)
+refresh()
+
+btn_a = tk.Button(window, text="A", command=refresh)
+btn_b = tk.Button(window, text="B", command=refresh)
 btn_a.pack(side=tk.LEFT)
 btn_b.pack(side=tk.LEFT)
-
-list = cuars.get_files(basedir, ".py")
-interf.show_text(list)
-interfimage = interf.image
-image = tk.PhotoImage(file="test.dir/image.png")
-image = interf.get_tkimage()
-canvas.create_image((0, 0), anchor=tk.NW, image=image)
 
 window.mainloop()
