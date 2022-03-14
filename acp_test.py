b86c4e66a49faaf2d724b32c09fa3df88425e1cc
Eric Duhamel <ericxdu23@gmail.com>
2022-03-14 07:20:35 +0000

    change adafruit test to use new module

--- acp_test.py
+++ acp_test.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+#    Copyright 2022 Eric Duhamel
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see
+#    <https://www.gnu.org/licenses/>.
 """
 This is a basic demonstration of CUARS using Adafruit CircuitPython to
 show the display on PiTFT display hardware. It's designed for display
@@ -7,78 +23,42 @@ specialized modules for custom displays configurations.
 """
 import os
 import sys
-#import subprocess
 import time
 
+from PIL import Image, ImageDraw, ImageFont
+
+from cuars import interf
 import adargb
-import cuars
 
 io = adargb.BoardIO()
 display = adargb.Display(135, 240)
-interf = cuars.Table(240, 135)
+
+table = interf.Table(240, 135, os.listdir(os.getcwd()))
+font = ImageFont.truetype("fonts/BebasNeue.otf", 22)
+image = Image.new("RGB", (240, 135))
+draw = ImageDraw.Draw(image)
 
 def main():
-    mark = 0
     print("Interface should appear on the RGB display now")
     while True:
-        dirname = os.getcwd()
-        if os.path.isdir(dirname):
-            dirlist = os.listdir(dirname)
-            pattern = colorize(dirname, dirlist)
-            s = interf.slice(dirlist, mark)
-            interf.set_list(dirlist[s:])
-            interf.set_pattern(pattern[s:])
-            interf.name = os.path.basename(dirname)
-            interf.mark = mark-s
-            interf.set_pager(mark+1, len(dirlist))
-            interf.render()
         # Display image.
-        image = interf.image
-        display.show(image, 90)
+        for badge in table.badges:
+            rect = (badge.x, badge.y,
+                    badge.x + badge.width, badge.y + badge.height)
+            bgcolor, color = badge.bgcolor, badge.color
+            name = badge.name
+            draw.rectangle(rect, outline=bgcolor, fill=bgcolor)
+            draw.text(rect, name, font=font, fill=color)
+        display.show(image, 270)
         # Accept input
         if not io.B.value and not io.A.value:  # both buttons pressed
-            print("cd home")
-            os.chdir(os.path.expanduser("~"))
+            print("restart")
         elif io.B.value and not io.A.value:  # button 1 pressed
-            mark = (mark+1) % len(dirlist)
+            print("select")
         elif io.A.value and not io.B.value:  # button 2 pressed
-            path = os.path.join(dirname, dirlist[mark])
-            if os.path.isdir(path):
-                print("cd: " + path)
-                os.chdir(path)
-                mark = 0
-            else:
-                print("\nFile " + str(mark+1) + " of " + str(len(dirlist)))
-                fileinfo(path)
+            print("start")
         # Wait for refresh
         time.sleep(0.1)
-
-def colorize(root, files):
-    """Return a color codes from a list of files"""
-    colors = []
-    for name in files:
-        path = os.path.join(root, name)
-        if os.path.islink(path): colors.append(3)
-        elif os.path.ismount(path): colors.append(4)
-        elif os.path.isdir(path): colors.append(1)
-        elif os.access(path, os.X_OK): colors.append(2)
-        else: colors.append(7)
-    return colors
-
-def fileinfo(path):
-    info = os.stat(path)
-    print("Dirname: " + os.path.dirname(path))
-    print("Basename: " + os.path.basename(path))
-    print("Realpath: " + os.path.realpath(path))
-    print("Stat")
-    print("  Mode: " + str(info.st_mode))
-    print("  Links: " + str(info.st_nlink))
-    print("  User: " + str(info.st_uid))
-    print("  Group: " + str(info.st_gid))
-    print("  Size: " + str(info.st_size))
-    print("  Accessed: " + time.ctime(info.st_atime))
-    print("  Modified: " + time.ctime(info.st_mtime))
-    print("Working: " + os.getcwd())
 
 if __name__ == "__main__":
     try:
@@ -87,4 +67,3 @@ if __name__ == "__main__":
         print("Keyboard Interrupt (Control-C)...")
     io.backlight.value = not io.backlight.value
     sys.exit()
-
